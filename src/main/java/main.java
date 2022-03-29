f661a39f4967a7788d491a7af74d53b5a76df171
kangzz <kangzz@ziroom.com>
2022-03-29 12:09:51 +0800

    gitignore111

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,7 @@ import util.PrintUtil;
 
 public class main {
     public static void main(String[] args) {
-        PrintUtil.printResult("两数相加", AddTwoNumbers.start());
+        //PrintUtil.printResult("两数相加", AddTwoNumbers.start());
         PrintUtil.printResult("电话号码的字母组合", LetterCombinations.letterCombinations("2345"));
         PrintUtil.printResult("比较APP版本号", CompareVersion.compareVersion("1.0.1","1"));
         PrintUtil.printResult("无重复字符的最长子串", LengthOfLongestSubstring.lengthOfLongestSubstring("134535"));
