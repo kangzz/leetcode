963e5f6ef35afa5c4cb3f1843aa894dc8fcfe881
kangzz <kangzz@ziroom.com>
2022-03-29 12:00:07 +0800

    gitignore

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,6 @@ public class main {
         //PrintUtil.printResult("两数相加",AddTwoNumbers.start());
         //PrintUtil.printResult("电话号码的字母组合",LetterCombinations.letterCombinations("2345"));
         //PrintUtil.printResult("比较APP版本号", CompareVersion.compareVersion("1.0.1","1"));
-        //PrintUtil.printResult("无重复字符的最长子串", LengthOfLongestSubstring.lengthOfLongestSubstring("134535"));
+        PrintUtil.printResult("无重复字符的最长子串", LengthOfLongestSubstring.lengthOfLongestSubstring("134535"));
     }
 }
