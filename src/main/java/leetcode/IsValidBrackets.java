9d165bdcb7ffeff996bf28e34fa125dc19fc3da7
kangzz <kangzz@ziroom.com>
2022-03-29 22:16:26 +0800

    字符串转整数 & 有效括号

---
+++ src/main/java/leetcode/IsValidBrackets.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * 有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class IsValidBrackets {
+    public static boolean isValidBrackets(String s) {
+        Stack<Character> stack = new Stack<>();
+        if(s.length() % 2 != 0){
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        char charV;
+        for (int i = 0; i < chars.length; i++) {
+            charV = chars[i];
+            if(charV == '(' || charV == '[' || charV == '{'){
+                stack.add(charV);
+            }else if(charV == ')'){
+                if(stack.isEmpty() || stack.peek() != '('){
+                    return false;
+                }
+                stack.pop();
+            } else if(charV == ']'){
+                if(stack.isEmpty() || stack.peek() != '['){
+                    return false;
+                }
+                stack.pop();
+            } else if(charV == '}'){
+                if(stack.isEmpty() || stack.peek() != '{'){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,5 +8,7 @@ public class main {
         PrintUtil.printResult("电话号码的字母组合", LetterCombinations.letterCombinations("23"));
         PrintUtil.printResult("比较APP版本号", CompareVersion.compareVersion("1.0.1","1"));
         PrintUtil.printResult("无重复字符的最长子串", LengthOfLongestSubstring.lengthOfLongestSubstring("134535"));
+        PrintUtil.printResult("字符串转换整数",MyAtoi.myAtoi("2147483648"));
+        PrintUtil.printResult("有效的括号",IsValidBrackets.isValidBrackets("()[]{}"));
     }
 }
